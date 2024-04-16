5c56db3d1cb936bae804daec8de548b81b057a47
Mohamed Karem <m.karem456@gmail.com>
2024-04-17 00:15:13 +0200

    edits

--- routes/api.php
+++ routes/api.php
@@ -56,7 +56,4 @@ Route:: as('site.')
         Route::get('portfolio-categories', PortfolioCategoryController::class)->name('portfolio-categories');
         Route::get('portfolios', PortfolioController::class)->name('portfolios');
         Route::post('contact', ContactUsController::class)->name('contact.store');
-        Route::get('contact_m',function (){
-            dd('a7a');
-        });
     });
