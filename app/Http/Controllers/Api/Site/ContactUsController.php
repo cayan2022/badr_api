28ed2e8acd73a4f6554162a637bb4bff10ba3eb6
Mohamed Karem <m.karem456@gmail.com>
2024-04-16 23:14:59 +0200

    edits

--- app/Http/Controllers/Api/Site/ContactUsController.php
+++ app/Http/Controllers/Api/Site/ContactUsController.php
@@ -6,20 +6,13 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\Site\CreateContactUsRequest;
 use App\Http\Resources\ContactUsResource;
 use App\Models\ContactUs;
-use Illuminate\Http\Request;
 
 class ContactUsController extends Controller
 {
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  CreateContactUsRequest  $request
-     * @return ContactUsResource
-     */
-    public function _invoke(CreateContactUsRequest $request)
+    public function __invoke(CreateContactUsRequest $createContactUsRequest): ContactUsResource
     {
-        $contactUs= ContactUs::create($request->validated());
-        if($request->hasFile('file') && $request->file('file')->isValid()){
+        $contactUs= ContactUs::create($createContactUsRequest->validated());
+        if($createContactUsRequest->hasFile('file') && $createContactUsRequest->file('file')->isValid()){
             $contactUs->addMediaFromRequest('file')
                 ->sanitizingFileName(fn($fileName)=>updateFileName($fileName))
                 ->toMediaCollection(ContactUs::MEDIA_COLLECTION_NAME);
