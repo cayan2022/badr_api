435c56f91d0dc19331d6be2fd90425cef8f5af63
Mohamed Karem <m.karem456@gmail.com>
2024-04-17 21:52:08 +0200

    edits

--- app/Http/Resources/ProjectResource.php
+++ app/Http/Resources/ProjectResource.php
@@ -17,6 +17,7 @@ class ProjectResource extends JsonResource
         $business_ar = [];
         $business_en = [];
         foreach ($this->businessDomains as $domain){
+            dd($domain->translate()->locale);
             if ($domain->translate()->locale == 'ar'){
                 $business_ar['id'] = $domain->id;
                 $business_ar['title'] = $domain->translate('ar')->title;
