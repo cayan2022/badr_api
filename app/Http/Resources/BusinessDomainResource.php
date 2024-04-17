236ce263e6d34c9ff285e89aeaacb629a6fb454a
Mohamed Karem <m.karem456@gmail.com>
2024-04-17 21:13:06 +0200

    edits

--- app/Http/Resources/BusinessDomainResource.php
+++ app/Http/Resources/BusinessDomainResource.php
@@ -6,17 +6,12 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class BusinessDomainResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
-     */
     public function toArray($request)
     {
         return [
             'id' => $this->id,
-            'title' => $this->title
+            'title_ar' => $this->translate('ar')->title,
+            'title_en' => $this->translate('en')->title,
         ];
     }
 }
