2e9e8601a3b91dfed24e5d6eba782d89fa815eae
Mohamed Karem <m.karem456@gmail.com>
2024-04-17 21:20:43 +0200

    edits

--- app/Http/Resources/BusinessDomainResource.php
+++ app/Http/Resources/BusinessDomainResource.php
@@ -8,6 +8,8 @@ class BusinessDomainResource extends JsonResource
 {
     public function toArray($request)
     {
+        $locale = app()->getLocale();
+        dd($locale);
         return [
             'id' => $this->id,
             'title' => $this->title,
